7c86667ff63b8c56914f725ac4cf62e3d2c96386
Smoke <68583856+Smoke-007@users.noreply.github.com>
2020-10-12 22:07:27 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -34,7 +34,7 @@ _add me in a supergroup and promote me as admin to let me get in action!
 
 
 HELP_STRINGS = """
-**SETTINGS**
+Hey there! My name is *{}*.
 _select one of the settings that you want to change.
 
 
